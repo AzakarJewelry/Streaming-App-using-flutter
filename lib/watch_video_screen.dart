6c70f24c76c4a8abdf1a0a805abb149656720e0b
d1o10 <vdiocampo009@gmail.com>
2025-02-28 11:24:49 +0800

    vertical

--- lib/watch_video_screen.dart
+++ lib/watch_video_screen.dart
@@ -3,27 +3,84 @@ import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
 class WatchVideoScreen extends StatefulWidget {
-  const WatchVideoScreen({super.key});
+  const WatchVideoScreen({Key? key}) : super(key: key);
 
   @override
   _WatchVideoScreenState createState() => _WatchVideoScreenState();
 }
 
 class _WatchVideoScreenState extends State<WatchVideoScreen> {
-  late VideoPlayerController _controller;
-  bool _controlsVisible = true;
-  Timer? _hideControlsTimer;
-  int _currentVideoIndex = 0;
-
+  final PageController _pageController = PageController(initialPage: 0);
   final List<String> _videoUrls = [
     'https://res.cloudinary.com/dlmeqb9qn/video/upload/v1740649505/videoplayback_xulk99.mp4',
     'https://res.cloudinary.com/dlmeqb9qn/video/upload/v1740639042/When_Your_Wife_Watches_a_Horror_Movie_yulong_Yangmiemie_yuyang___Short_Drama_Zone_ezo9fv.mp4'
   ];
 
   @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: PageView.builder(
+        controller: _pageController,
+        scrollDirection: Axis.vertical,
+        itemCount: _videoUrls.length,
+        itemBuilder: (context, index) {
+          return VideoPlayerPage(
+            videoUrl: _videoUrls[index],
+            onNext: () {
+              if (index < _videoUrls.length - 1) {
+                _pageController.nextPage(
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.easeIn,
+                );
+              }
+            },
+            onPrevious: () {
+              if (index > 0) {
+                _pageController.previousPage(
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.easeIn,
+                );
+              }
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class VideoPlayerPage extends StatefulWidget {
+  final String videoUrl;
+  final VoidCallback onNext;
+  final VoidCallback onPrevious;
+
+  const VideoPlayerPage({
+    Key? key,
+    required this.videoUrl,
+    required this.onNext,
+    required this.onPrevious,
+  }) : super(key: key);
+
+  @override
+  _VideoPlayerPageState createState() => _VideoPlayerPageState();
+}
+
+class _VideoPlayerPageState extends State<VideoPlayerPage> {
+  late VideoPlayerController _controller;
+  bool _controlsVisible = true;
+  Timer? _hideControlsTimer;
+
+  @override
   void initState() {
     super.initState();
-    _initializeVideoPlayer(_videoUrls[_currentVideoIndex]);
+    _initializeVideoPlayer(widget.videoUrl);
   }
 
   void _initializeVideoPlayer(String url) {
@@ -57,7 +114,6 @@ class _WatchVideoScreenState extends State<WatchVideoScreen> {
     setState(() {
       _controlsVisible = !_controlsVisible;
     });
-
     if (_controlsVisible) {
       _startHideControlsTimer();
     } else {
@@ -76,105 +132,94 @@ class _WatchVideoScreenState extends State<WatchVideoScreen> {
     });
   }
 
-  void _playNextVideo() {
-    if (_currentVideoIndex < _videoUrls.length - 1) {
-      _currentVideoIndex++;
-      _controller.dispose();
-      _initializeVideoPlayer(_videoUrls[_currentVideoIndex]);
-    }
-  }
-
-  void _playPreviousVideo() {
-    if (_currentVideoIndex > 0) {
-      _currentVideoIndex--;
-      _controller.dispose();
-      _initializeVideoPlayer(_videoUrls[_currentVideoIndex]);
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black, // Set background color to black
-      body: GestureDetector(
-        onTap: _toggleControlsVisibility,
-        child: Stack(
-          children: [
-            // Video Player
-            _controller.value.isInitialized
-                ? Center(
-                    child: AspectRatio(
-                      aspectRatio: _controller.value.aspectRatio,
-                      child: VideoPlayer(_controller),
-                    ),
-                  )
-                : const Center(child: CircularProgressIndicator()),
-
-            // Text and Icons Overlay
-            Positioned(
-              bottom: 70,
-              left: 20,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '@username',
-                    style: TextStyle(color: Colors.white, fontSize: 18),
+    return GestureDetector(
+      onTap: _toggleControlsVisibility,
+      child: Stack(
+        children: [
+          // Video Player
+          _controller.value.isInitialized
+              ? Center(
+                  child: AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    child: VideoPlayer(_controller),
                   ),
-                  SizedBox(height: 10),
-                  Text(
-                    'Video description goes here...',
-                    style: TextStyle(color: Colors.white, fontSize: 14),
-                  ),
-                ],
-              ),
+                )
+              : const Center(child: CircularProgressIndicator()),
+          // Overlay Text and Icons
+          Positioned(
+            bottom: 70,
+            left: 20,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  '@username',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Video description goes here...',
+                  style: TextStyle(color: Colors.white, fontSize: 14),
+                ),
+              ],
             ),
-            Positioned(
-              bottom: 60,
-              right: 20,
-              child: Column(
-                children: [
-                  Icon(Icons.favorite, color: Colors.white, size: 30),
-                  SizedBox(height: 10),
-                  Icon(Icons.comment, color: Colors.white, size: 30),
-                  SizedBox(height: 10),
-                  Icon(Icons.share, color: Colors.white, size: 30),
-                ],
-              ),
+          ),
+          Positioned(
+            bottom: 60,
+            right: 20,
+            child: Column(
+              children: const [
+                Icon(Icons.favorite, color: Colors.white, size: 30),
+                SizedBox(height: 10),
+                Icon(Icons.comment, color: Colors.white, size: 30),
+                SizedBox(height: 10),
+                Icon(Icons.share, color: Colors.white, size: 30),
+              ],
             ),
-
-            // Video Controls (Visible on Tap)
-            if (_controlsVisible)
-              Positioned.fill(
-                child: Container(
-                  color: Colors.black.withOpacity(0.3), // Dims background when controls are visible
-                  child: Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        IconButton(
-                          icon: Icon(Icons.skip_previous, color: Colors.white, size: 50),
-                          onPressed: _playPreviousVideo,
+          ),
+          // Video Controls (Next/Previous/Play-Pause)
+          if (_controlsVisible)
+            Positioned.fill(
+              child: Container(
+                color: Colors.black.withOpacity(0.3),
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        icon: const Icon(
+                          Icons.skip_previous,
+                          color: Colors.white,
+                          size: 50,
                         ),
-                        IconButton(
-                          icon: Icon(
-                            _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
-                            color: Colors.white,
-                            size: 60,
-                          ),
-                          onPressed: _togglePlayPause,
+                        onPressed: widget.onPrevious,
+                      ),
+                      IconButton(
+                        icon: Icon(
+                          _controller.value.isPlaying
+                              ? Icons.pause
+                              : Icons.play_arrow,
+                          color: Colors.white,
+                          size: 60,
                         ),
-                        IconButton(
-                          icon: Icon(Icons.skip_next, color: Colors.white, size: 50),
-                          onPressed: _playNextVideo,
+                        onPressed: _togglePlayPause,
+                      ),
+                      IconButton(
+                        icon: const Icon(
+                          Icons.skip_next,
+                          color: Colors.white,
+                          size: 50,
                         ),
-                      ],
-                    ),
+                        onPressed: widget.onNext,
+                      ),
+                    ],
                   ),
                 ),
               ),
-          ],
-        ),
+            ),
+        ],
       ),
     );
   }
